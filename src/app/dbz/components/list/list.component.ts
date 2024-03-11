223c0996aeaa36d5e1f493df6db319c24006bdbb
kenny1707 <72925470+kenny1707@users.noreply.github.com>
2024-03-10 22:50:25 -0500

    Seccion 5 finalizada

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    id: 'uuid',
+    name: 'Trunks',
+    power: 10
+  }];
+
+  // onDeleteID
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?:string): void {
+    //TODO: Emitir el ID del personaje
+    // console.log({id});
+    this.onDelete.emit(id);
+
+  }
+
+}
