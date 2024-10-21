43038f792a267b58dd2a0f6ce3d8bdc5c2442422
Kenny Carmona <kennyharold02@gmail.com>
2024-10-20 19:43:36 -0500

    Lista para publicarlo

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name:'',
+    power: 0
+  }]
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  // onDeleteCharacter( index:number ):void {
+  //   this.onDelete.emit( index );
+  // }
+  onDeleteCharacter( id?:string ):void {
+
+    if (!id) return;
+
+    this.onDelete.emit( id );
+  }
+
+}
